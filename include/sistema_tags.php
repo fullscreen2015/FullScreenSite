d33d1ba9f8d36629273136d17d97d0e3e1c04246
fullscreen2015 <pedro@fullscreen.com.br>
2015-05-11 17:25:43 -0300

    Tags
    correção de tags.php

--- include/sistema_tags.php
+++ include/sistema_tags.php
@@ -1,4 +1,4 @@
-<?php
+<?
 
 
   function somenteTexto($string)
@@ -22,7 +22,7 @@
         //Come�a a criar o novo texto resumido.
         $novoTexto = "";
         //Acrescenta palavra por palavra na string enquanto ela
-        //não exceder o tamanho m�ximo do resumo
+        //não exceder o tamanho máximo do resumo
         for($i = 0; $i <count($vetorPalavras); $i++):
             $totalCaracteres += strlen(" ".$vetorPalavras[$i]);
             if($totalCaracteres <= $limite)
@@ -183,53 +183,53 @@ if($_REQUEST['conteudo'] == "produtos")
   
     $links[11]="HOME";
     $links[12]="principal";
-    $links[13]="Automa��o Comercial - Fullscreen - Sistema de Automa��o";
-    $links[14]="automa��o comercial, sistema de automa��o comercial,nota fiscal eletr�nica, fullscreen, sistema de controle comercial";
-    $links[15]="soluções em automa��o comercial, como nota fiscal eletr�nica e cupom fiscal. Tenha total controle operacional sobre seu com�rcio.";
+    $links[13]="Automação Comercial - Fullscreen - Sistema de Automação";
+    $links[14]="automação comercial, sistema de automação comercial,nota fiscal eletrônica, fullscreen, sistema de controle comercial";
+    $links[15]="soluções em automação comercial, como nota fiscal eletrônica e cupom fiscal. Tenha total controle operacional sobre seu comércio.";
 
     $links[21]="EMPRESA";
     $links[22]="empresa";
-    $links[23]="Fullscreen Automa��o Comercial - soluções para seu com�rcio" ;
-    $links[24]="empresa de automa��o comercial, fullscreen, empresa de automa��o, empresas de automa��o comercial, automa��o comercial rj, servi�os de hardware" ;
-    $links[25]="A Fullscreen Automa��o Comercial, atua no desenvolvimento de soluções corporativas para empresas que buscam aprimorar seu controle interno." ;
+    $links[23]="Fullscreen Automação Comercial - soluções para seu comércio" ;
+    $links[24]="empresa de automação comercial, fullscreen, empresa de automação, empresas de automação comercial, automação comercial rj, serviços de hardware" ;
+    $links[25]="A Fullscreen Automação Comercial, atua no desenvolvimento de soluções corporativas para empresas que buscam aprimorar seu controle interno." ;
   
     $links[31]="soluções";
     $links[32]="solucoes";
-    $links[33]="Programa de Automa��o Comercial - Fullscreen Tecnologia de Computa��o" ;
-    $links[34]="automa��o e controle industrial, controle automa��o, controle e automa��o, programa de automa��o comercial, sistema integrado comercial" ;
-    $links[35]="Agilize seus processos manuais com o sistema de automa��o comercial. soluções que resultam o controle operacional total do seu neg�cio." ;
+    $links[33]="Programa de Automação Comercial - Fullscreen Tecnologia de Computação" ;
+    $links[34]="automação e controle industrial, controle automação, controle e automação, programa de automação comercial, sistema integrado comercial" ;
+    $links[35]="Agilize seus processos manuais com o sistema de automação comercial. soluções que resultam o controle operacional total do seu neg�cio." ;
 
     $links[41]="PRODUTOS";
     $links[42]="produtos";
     $links[43]="Produtos Fullscreen - Hardware e Sofware" ;
-    $links[44]="produtos de automa��o comercial, software de automa��o comercial, produtos fullscreen, programa para controle de estoque, impressora fiscal" ;
-    $links[45]="Produtos Fullscreen para o seu controle operacional. Hardwares e softwares para agilizar os processos manuais do seu com�rcio." ;
+    $links[44]="produtos de automação comercial, software de automação comercial, produtos fullscreen, programa para controle de estoque, impressora fiscal" ;
+    $links[45]="Produtos Fullscreen para o seu controle operacional. Hardwares e softwares para agilizar os processos manuais do seu comércio." ;
 
     $links[51]="CLIENTES";
     $links[52]="clientes";
-    $links[53]="Clientes Fullscreen - Automa��o Comercial" ;
-    $links[54]="clientes fullscreen, amil esportes, big m�quinas, d�borah braune, estilo livre" ;
-    $links[55]="Atendemos aos segmentos de Atacado/Distribui��o, Auto-Pe�as, pequenos com�rcios at� grandes lojas de departamentos. conheça nossos clientes." ;
+    $links[53]="Clientes Fullscreen - Automação Comercial" ;
+    $links[54]="clientes fullscreen, big máquinas, déborah braune, estilo livre" ;
+    $links[55]="Atendemos aos segmentos de Atacado/Distribuição, Auto-Peças, pequenos comércios até grandes lojas de departamentos. conheça nossos clientes." ;
 
-    $links[61]="�REA DO CLIENTE";
-    $links[62]="area-cliente-login";
-    $links[63]="�rea do Clientes" ;
-    $links[64]="�rea do cliente, acesso clientes" ;
-    $links[65]="já � cliente Fullscreen? Ent�o acesse aqui com seu login e senha." ;
+    $links[61]="ÁREA DO CLIENTE";
+    $links[62]="Área-cliente-login";
+    $links[63]="Área do Clientes" ;
+    $links[64]="Área do cliente, acesso clientes" ;
+    $links[65]="já é cliente Fullscreen? Então acesse aqui com seu login e senha." ;
 
     $links[71]="CONTATO";
     $links[72]="contato";
     $links[73]="Contato Fullscreen - Fale Conosco" ;
     $links[74]="contato fullscreen, fale conosco fullscreen, entre em contato" ;
-    $links[75]="Fale conosco Fullscreen. Deixe sua sugest�o, d�vida ou cr�tica que responderemos o mais breve poss�vel.." ;
+    $links[75]="Fale conosco Fullscreen. Deixe sua sugestão, dúvida ou crítica que responderemos o mais breve possível.." ;
 
 
     $numero_links = 7;
   
 
-    $titulo = "Automa��o Comercial - Fullscreen - Sistema de Automa��o";
-    $palavras_chave = "automa��o comercial, sistema de automa��o comercial,nota fiscal eletr�nica, fullscreen, sistema de controle comercial";
-    $descricao = "soluções em automa��o comercial, como nota fiscal eletr�nica e cupom fiscal. Tenha total controle operacional sobre seu com�rcio.";
+    $titulo = "Automação Comercial - Fullscreen - Sistema de Automação";
+    $palavras_chave = "automação comercial, sistema de automação comercial,nota fiscal eletrônica, fullscreen, sistema de controle comercial";
+    $descricao = "soluções em automação comercial, como nota fiscal eletrônica e cupom fiscal. Tenha total controle operacional sobre seu comércio.";
 
 
 
