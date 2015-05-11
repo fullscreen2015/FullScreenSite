03d46db156b3fb8c955a7ed3bd6c2369297ceccf
fullscreen2015 <pedro@fullscreen.com.br>
2015-05-11 17:42:57 -0300

    Correção Textual
    correção textual em soluções

--- include/conteudo_solucoes.php
+++ include/conteudo_solucoes.php
@@ -3,13 +3,13 @@
 		<div class="caixa_solucoes">
 			<h1>soluções</h1>
 			<div class="caixa_automacao">
-				<p class="texto_automacao">AUTOMA��O COMERCIAL</p>
+				<p class="texto_automacao">AUTOMAÇÃO COMERCIAL</p>
 			</div>
           
-          <p class="texto_solucoes">Automa��o comercial � a aplica��o de m�todos e ferramentas para automatizar processos comerciais, isso �, mecanizar e agilizar processos manuais, alcan�ando total efici�ncia. </p>
-          <p class="texto_solucoes">A integra��o entre o homem e a m�quina somados a gest�o, busca reduzir a m�o-de-obra e despesas, al�m de ger�ncia e controle operacional sobre um com�rcio. Com a automa��o, tarefas pass�veis de erros, como: c�lculo e digita��o de pre�os, quantidades, preenchimento de um cheque, emiss�o de nota fiscal; ficam mais seguras e eficientes. Melhorando o trabalho dos funcion�rios e o atendimento aos clientes. </p>
-          <p class="texto_solucoes">Atualmente as leis brasileiras est�o ficando mais complexas e exigentes no que se refere a automa��o comercial, exigindo da empresa desenvolvedora bem como do usuário final r�gidos controles de forma a evitar a sonega��o fiscal. </p>
-          <p class="texto_solucoes">A �Full Screen� possui software pr�prio desenvolvido de acordo com as novas leis vigentes no pa�s devidamente homologado pelos agentes credenciados pelas receitas estaduais e federais.</p>
+          <p class="texto_solucoes">Automação comercial é a aplicação de métodos e ferramentas para automatizar processos comerciais, isso é, mecanizar e agilizar processos manuais, alcançando total eficiência.</p>
+          <p class="texto_solucoes">A integração entre o homem e a máquina somados a gestão, busca reduzir a mão-de-obra e despesas, além de gerência e controle operacional sobre um comércio. Com a automação, tarefas passíveis de erros, como: cálculo e digitação de preço, quantidades, preenchimento de um cheque, emissão de nota fiscal; ficam mais seguras e eficientes. Melhorando o trabalho dos funcionários e o atendimento aos clientes.</p>
+          <p class="texto_solucoes">Atualmente as leis brasileiras estão ficando mais complexas e exigentes no que se refere a automação comercial, exigindo da empresa desenvolvedora bem como do usuário final rígidos controles de forma a evitar a sonegação fiscal.</p>
+          <p class="texto_solucoes">A Full Screen possui software próprio desenvolvido de acordo com as novas leis vigentes no país devidamente homologado pelos agentes credenciados pelas receitas estaduais e federais.</p>
 
 		</div>
 
@@ -24,9 +24,9 @@
 				<p class="texto_automacao">VDMPlus</p>
 			</div>
           
-          <p class="texto_solucoes">O VDM � um aplicativo desenvolvido para Tablets e Smartphones, que tenham o Android como sistema operacional. Destina-se a atender empresas que trabalham com vendas externas (atacadistas / distribuidores). </p>
+          <p class="texto_solucoes">O VDM é um aplicativo desenvolvido para Tablets e Smartphones, que tenham o Android como sistema operacional. Destina-se a atender empresas que trabalham com vendas externas (atacadistas / distribuidores). </p>
 
-          <p class="texto_solucoes">através de um programa que funciona em um computador fazemos a comunica��o entre computador e os dispositivos (tablete ou Smartphone). Podemos dizer que esse programa � a matriz. E � tamb�m através dele que instalamos e configuramos o VDM nos Tablets e Smartphones. </p>
+          <p class="texto_solucoes">através de um programa que funciona em um computador fazemos a comunicação entre computador e os dispositivos (tablete ou Smartphone). Podemos dizer que esse programa é a matriz. E também através dele que instalamos e configuramos o VDM nos Tablets e Smartphones. </p>
 
 
 		</div>
@@ -55,20 +55,20 @@
 <div class="text_vdm">
 
  <p class="texto_solucoes">O VDM fornece diversas funcionalidades para seu usuário, tais como:</p>
- <p class="p_destaque_solucoes">� &nbsp; &nbsp; &nbsp;	Realiza��o de Vendas</p>
+ <p class="p_destaque_solucoes">� &nbsp; &nbsp; &nbsp;	Realização de Vendas</p>
  <p class="p_destaque_solucoes">� &nbsp; &nbsp; &nbsp;	Consulta de Vendas Realizadas</p>
  <p class="p_destaque_solucoes">� &nbsp; &nbsp; &nbsp;	Cadastro de Clientes</p>
  <p class="p_destaque_solucoes">� &nbsp; &nbsp; &nbsp;	Consulta de Clientes</p>
- <p class="p_destaque_solucoes">� &nbsp; &nbsp; &nbsp;	Consulta de T�tulos em Aberto</p>
- <p class="p_destaque_solucoes">� &nbsp; &nbsp; &nbsp;	Consulta de T�tulos Pagos</p>
- <p class="p_destaque_solucoes">� &nbsp; &nbsp; &nbsp;	Lan�amento de Cr�ditos e D�bitos para Clientes</p>
- <p class="p_destaque_solucoes">� &nbsp; &nbsp; &nbsp;	Consulta de Cr�ditos e D�bitos dos Clientes</p>
+ <p class="p_destaque_solucoes">� &nbsp; &nbsp; &nbsp;	Consulta de Títulos em Aberto</p>
+ <p class="p_destaque_solucoes">� &nbsp; &nbsp; &nbsp;	Consulta de Títulos Pagos</p>
+ <p class="p_destaque_solucoes">� &nbsp; &nbsp; &nbsp;	Lan�amento de Créditos e Débitos para Clientes</p>
+ <p class="p_destaque_solucoes">� &nbsp; &nbsp; &nbsp;	Consulta de Créditos e Débitos dos Clientes</p>
  <p class="p_destaque_solucoes">� &nbsp; &nbsp; &nbsp;	Consulta de Produtos</p>
 
 </div>
 
 <div class="text_bottom_solucoes">
-	<p>Uma grande funcionalidade do VDM � o fato dele trabalhar com m�dulos. Isso significa que de acordo com o m�dulo que o sistema for configurado, a acessibilidade do usuário �s funcionalidades � restringida ou ampliada. O VDM apresenta os seguintes m�dulos: <span>Gerencial Total, Gerencial Limitado, Vendas Total, Vendas Limitado e Supervisor</span>. Cada m�dulo possui fun��es espec�ficas com ou sem restri��es pr�-definidas. O sistema VDM ainda possui configurações personalizadas, que se adaptam de acordo com as necessidades de cada cliente.</p>
+	<p>Uma grande funcionalidade do VDM é o fato dele trabalhar com módulos. Isso significa que de acordo com o módulo que o sistema for configurado, a acessibilidade do usuário bem como ás funcionalidades são restringidas ou ampliadas. O VDM apresenta os seguintes módulos: <span>Gerencial Total, Gerencial Limitado, Vendas Total, Vendas Limitado e Supervisor</span>. Cada módulo possui funções específicas com ou sem restrições pré-definidas. O sistema VDM ainda possui configurações personalizadas, que se adaptam de acordo com as necessidades de cada cliente.</p>
 </div>
 
 
